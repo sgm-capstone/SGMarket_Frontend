8bba7db158f633a4efc70c85e1d7e37d788885db
lzvckaya@gmail.com <lzvckaya@gmail.com>
2025-05-31 23:54:19 +0900

    테스트 결제 코드 추가

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.r0ug8ac1db"
+    "revision": "0.538v8t15cm8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/singup/RegisterComplete.tsx
+++ src/pages/singup/RegisterComplete.tsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import { FaChevronLeft } from "react-icons/fa";
 
 export default function RegisterComplete() {
   const navigate = useNavigate();
